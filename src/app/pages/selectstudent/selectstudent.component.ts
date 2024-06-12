15620865fa63dd76f325c9d4a7c8a718c73abd47
Ashiklawrence <ashiklawrence12@gmail.com>
2024-06-12 10:18:04 +0530

    minor_style_fixes

--- src/app/pages/selectstudent/selectstudent.component.ts
+++ src/app/pages/selectstudent/selectstudent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-selectstudent',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./selectstudent.component.scss']
 })
 export class SelectstudentComponent {
+
+  constructor(private _router : Router){
+
+      
+  }
+  handleClick(){
+    this._router.navigate(["payment"])
+  }
 
 }
